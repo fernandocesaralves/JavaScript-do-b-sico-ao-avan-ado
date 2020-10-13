da58e04003c97a5143d957ece10542f714b505ab
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-13 18:50:54 -0300

    aula 21 - Strings

---
+++ 2_tipo-de-dados-e-operadores/aula21/script.js
@@ -0,0 +1,5 @@
+console.log("Primeira linha \nsegunda linha");
+console.log("O meu nome é 'Fernando'");
+console.log('O nome dela é "Thais"');
+console.log(`A multiplicação de 5 por 3 é ${5*3}`);
+console.log("O " + "meu " + "nome " + "é " + "Fernando");
