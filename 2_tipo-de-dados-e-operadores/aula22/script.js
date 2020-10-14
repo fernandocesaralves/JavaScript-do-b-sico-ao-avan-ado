1c25b2b947f142a27a0ea844a5efe9c1ac1df4d7
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-14 14:26:15 -0300

    Booleans

---
+++ 2_tipo-de-dados-e-operadores/aula22/script.js
@@ -0,0 +1,10 @@
+// Boleans
+
+//Serve para guardar um valor de uma comparação
+
+// Os únicos valores possíveis são: True (Verdadeiro) e False (Falso)
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof (10 > 2));
+console.log(typeof (5 > 200));
---
+++ 2_tipo-de-dados-e-operadores/aula23/script.js
@@ -0,0 +1,24 @@
+//Booleans: Comparações
+
+    // Maior que >
+
+    // Menor que <
+
+    // Maior ou igual >= 
+
+    // Menor ou igual <= 
+
+    // Igual ==
+
+    // Diferente != 
+
+    //Idêntico ===
+
+console.log(1 > 2);
+console.log(5 < 10);
+console.log(3 >= 3);
+console.log(5 <= 4);
+console.log(5 == 4);
+console.log('Fernando' != 'Fernando');
+console.log(3 === '3');
+console.log(4 === 4);
