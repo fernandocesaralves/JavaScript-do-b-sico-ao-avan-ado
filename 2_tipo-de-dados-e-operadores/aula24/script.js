78f4e07223991aae6a0c406202a58fe091f7f910
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-15 12:39:13 -0300

    Operadores logicos exemplos praticos

---
+++ 2_tipo-de-dados-e-operadores/aula24/script.js
@@ -0,0 +1,8 @@
+//Por meio de uma comparação resultam em um Boolean
+
+// && - and -> para ser true, os dois 'lados' da comparação precisam ser true
+
+// || - or -> para ser true, basta um dos 'lados' da comparação ser true;
+
+// ! - not -> inverter os valores (true vira false);
+
---
+++ 2_tipo-de-dados-e-operadores/aula25/script.js
@@ -0,0 +1,11 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+
+console.log(3 == 3 && 'Fernando' == 'Fernando');
+console.log('Felipe' == 'João' || 3 == 5);
+
+console.log(!(!(true && true)));
+
+console.log(true && true);
+console.log(false || false);
+
