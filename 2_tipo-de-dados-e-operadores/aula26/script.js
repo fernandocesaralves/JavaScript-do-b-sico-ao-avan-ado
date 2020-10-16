df76b0ecf1ca314ce87011f4e186f0dac9b74cc3
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-16 12:01:04 -0300

    Empty values e operadores ternarios

---
+++ 2_tipo-de-dados-e-operadores/aula26/script.js
@@ -0,0 +1,12 @@
+//Booleans: Operadores ternários 
+
+//Faz comparativo em apenas uma linha
+
+//Não é muito utilizado e pode deixar o código complicado de ler
+
+//Não vale a pena ser utilizado
+
+console.log(5 > 2 ? 'É maior' : 'É menos');
+console.log(false ? 5 : 4);
+console.log("Fernando" == "Fernando" ? "Olá Fernando" : "Você não é o Fernando");
+console.log(1 < 2 ? 'É menor' : 'É maior');
---
+++ 2_tipo-de-dados-e-operadores/aula27/script.js
@@ -0,0 +1,10 @@
+// Empty values 
+
+// Temos duas palavras reservadas da linguagem para estes casos: 
+
+// udefined e null 
+
+// Sempre que você se deparar com um desses valores significa que os valores não existem
+
+console.log(null);
+console.log(undefined);
