86404394378543bb512c44dda3abfe04beb5db6f
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-19 10:29:01 -0300

    Aula 28 - conversao automatica entre tipos

---
+++ 2_tipo-de-dados-e-operadores/aula28/script.js
@@ -0,0 +1,7 @@
+// Conversão de tipo automático
+
+// o JavaScript em determinadas operações, converte silenciosamente o tipo do resultado final da operação, veja:
+
+console.log(typeof("123" + 4));
+console.log(typeof("30" - 20));
+console.log(typeof("fern" * 3));
