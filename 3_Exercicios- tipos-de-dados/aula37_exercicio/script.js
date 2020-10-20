6c13bb01145f2d74ccc1ddb54876ae0ed8ac2510
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-20 10:13:15 -0300

    Exercicio 4 - Operadores logicos

---
+++ 3_Exercicios- tipos-de-dados/aula37_exercicio/script.js
@@ -0,0 +1,9 @@
+// Escreva três comprarações com operadores lógicos
+
+// Com And, Or e Not
+
+console.log(4 < 5 && 5 < 6);
+
+console.log(7 >= 8 || 3 > 4);
+
+console.log( !(4 > 3) );
---
+++ 3_Exercicios- tipos-de-dados/aula38_solucao/script.js
@@ -0,0 +1,5 @@
+console.log( 3 == 3 && 4 > 1 );
+
+console.log( 4 <= 2 || "Fernando" == "João" );
+
+console.log( !(2 === 2) );
