67968bbda44013e07eed010f5217107bcdf0a977
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-21 12:34:33 -0300

    Variaveis

---
+++ 4_Estruturas_de_Programacao/aula44/script.js
@@ -0,0 +1,25 @@
+let nome = "Fernando";
+
+console.log(nome);
+console.log(`Meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+nome = "João";
+
+console.log(nome);
+
+laranjas = 10;
+
+console.log(laranjas);
+
+laranjas = "Laranja";
+
+console.log(laranjas);
+
+let um = 1, dois = 2, tres = 3; // Não aconselhável, pois dificulta na leitura do código
+
+console.log(um + dois + tres);
+
