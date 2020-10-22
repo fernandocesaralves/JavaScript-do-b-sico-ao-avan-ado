6578e8f05e7c7b285c32918cf2d6ea8e9eb8a016
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-22 11:44:21 -0300

    Outras formas de declarar variaveis e convencoes

---
+++ 4_Estruturas_de_Programacao/aula45/script.js
@@ -0,0 +1,8 @@
+var nome = "Fernando";
+
+const ip = "127.0.0.1";
+
+console.log(nome);
+console.log(ip);
+
+ip = "0.1.2.8"; // Não é possível atribuir um novo valor para uma constante (const)
---
+++ 4_Estruturas_de_Programacao/aula46/script.js
@@ -0,0 +1,25 @@
+// Variáveis não podem ser iniciadas com números (let 2teste);
+
+// Mas pode terminar com número (let teste9 = 'testando');
+
+// Pode ter $ ou _, mas não outros caracteres especiais (let $nome, _nome);
+
+// Mas não pode ter pontuação ou outros especiais (let @teste);
+
+// Podemos iniciar com a letra maiúscula (let Nome = 'Fernando');
+
+// Ou usar camelCase (let meuPrimeiroNome = 'Fernando');
+
+//let 3nome = 'teste'; //Erro pois não pode começar com número
+
+let $nome = 'teste2';
+
+let _nome = 'teste3';
+
+//let @nome = 'teste4'; //Erro pois não pode começar por outros caracteres especiais com exceção do _ e $
+
+//let !nome = 'teste4'; //Erro pois não pode começar por outros caracteres especiais com exceção do _ e $
+
+let Nome = 'teste5';
+
+let seuNome = 'teste6'; //camelCase
