71b4563847aa6d5b96a8f233aaa0bae3e281aae3
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-22 21:23:56 -0300

    Palavras reservadas Javascript

---
+++ 4_Estruturas_de_Programacao/aula47/script.js
@@ -0,0 +1,11 @@
+//let if = 'teste'; // Uso de palavras reservadas vai apresentar erro 
+
+//let function = 'teste';
+
+let functionTest = 'teste'; //Porém, podemos usar esse nome reservador + alguma letra ou número
+
+let function1 = 'teste2';
+
+console.log(functionTest);
+
+console.log(function1);
