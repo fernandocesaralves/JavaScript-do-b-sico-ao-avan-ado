f22037118988019e8e2fdfff2fe4f956af38b02a
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-23 18:07:33 -0300

    funcoes - alert e Math

---
+++ 4_Estruturas_de_Programacao/aula51/script.js
@@ -0,0 +1,6 @@
+// Exibi uma mensagem na tela do usuário
+
+// É muito pouco utilizada, pois é considerada muito invasiva 
+
+alert("Esta é a mensagem!");
+alert("Esta é a mensagem 2");
---
+++ 4_Estruturas_de_Programacao/aula52/script.js
@@ -0,0 +1,19 @@
+// Função utilizada para expressões/cálculos matemáticos
+
+// É muito utilizada
+
+let maior = Math.max(6,12,28,55,8); // Math.max irá verificar qual é o maior número
+
+console.log(maior);
+
+let menor = Math.min(6,12,28,55,8); // Math.min irá verificar qual é o menor número
+
+console.log(menor);
+
+let arredondar = Math.round(5.298654321); // Math.round irá arredondar um número decimal, para cima ou para baixo, de acordo com as casas decimais
+
+console.log(arredondar);
+
+let arredondarParaCima = Math.ceil(5.203938474); // Math.ceil irá arredondar para cima independente das casas decimais serem baixas ou altas
+
+console.log(arredondarParaCima);
