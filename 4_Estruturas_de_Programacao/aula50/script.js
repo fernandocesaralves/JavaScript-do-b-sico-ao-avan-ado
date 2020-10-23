540ccd97d7e7b2e18f0cf46014bc17dc070595b4
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-23 12:02:19 -0300

    Funcao built-in prompt

---
+++ 4_Estruturas_de_Programacao/aula50/script.js
@@ -0,0 +1,13 @@
+// Função built-in
+
+// Recebe de um input 
+
+// Função PROMPT
+
+let idade = prompt("Qual é a sua idade?");
+
+console.log(idade);
+
+let nome = prompt("Qual é o seu nome?");
+
+console.log(`O seu nome é ${nome}`);
