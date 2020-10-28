59dbc85fa2e56c9b7d5d6ecb433644daf12f289b
Fernando Cesar <fernandocesar.alves@hotmail.com>
2020-10-28 12:27:21 -0300

    do while e for

---
+++ 4_Estruturas_de_Programacao/aula60/script.js
@@ -0,0 +1,10 @@
+// Bem semelhante ao while, porém sua estrutura muda um pouco
+
+// Este método quase não é utilizado 
+
+let x = 100;
+
+do {
+    console.log(x/2); // Primeiro executa
+    x -= 5;
+} while ( x >= 0 ); // Depois verifica
---
+++ 4_Estruturas_de_Programacao/aula61/script.js
@@ -0,0 +1,9 @@
+// Semelhante ao while, porém amplamente utilizado
+
+// É a estrutura de repetição mais fácil de compreender, tente sempre preferir por ela.
+
+// Com ela tudo pode ser declarada em uma linha, dentro do for
+
+for ( let i = 0; i < 100; i += 3 ) {
+    console.log( `A soma de i com 2 é: ${i + 2}` );
+}
