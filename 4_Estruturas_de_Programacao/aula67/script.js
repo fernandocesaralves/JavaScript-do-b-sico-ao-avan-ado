0d858210358a0c59bd75f6002d5b887b7b176d4a
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-10-31 17:09:18 -0300

    Boas praticas de declaracao de variaveis e comentarios

---
+++ 4_Estruturas_de_Programacao/aula67/script.js
@@ -0,0 +1,9 @@
+// Um pouco mais sobre declaração de variáveis 
+
+let carrovermelhogrande // Esse tipo não pode ser usado, pois é muito difícil de ler 
+
+let carro_vermelho_grande // Até poderia ser usado, mas não é legal estéticamente
+
+let CarroVermelhoGrande // Não pode ser usado pois há uma conversão de que somente classes devem começar por letra maiúscula
+
+let carroVermelhoGrande // Esse é o tipo ideal de se declarar uma variável 
---
+++ 4_Estruturas_de_Programacao/aula68/script.js
@@ -0,0 +1,18 @@
+// Comentários 
+
+// São usados para explicar o que está acontecendo no código 
+
+// Ou também para testar se algum código está afetando o sistema 
+
+// Completamente ignorado pelo interpretador de código 
+
+// Uma linha e multi-linha 
+
+/* 
+    Esse é um 
+    comentários 
+    de multiplas
+    Linhas 
+*/
+
+// console.log("Teste");
