8cadfd8763abae8d59d69d2d06af7b93fda437a2
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-11-20 20:39:34 -0300

    Mais sobre arrays

---
+++ 8_Arrays-e-Objetos/aula131/script.js
@@ -0,0 +1,21 @@
+// Podemos acessar um array pelo seu índice, por exemplo arr[1]
+
+// Lembrando que o primeiro índice sempre é o 0, ou seja, para acessar o primeiro elemento: arr[0];
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+let nomes = ['Fernando', 'Filomena', 'João', 'Thais'];
+
+let bool = [true, false, true, false];
+
+
+let misturado = [1, 'Fernando', true];
+
+
+console.log(arr[1]); // Acesssando o segundo elemento
+
+console.log(nomes[0]); // Acessando o primeiro elemento 
+
+console.log(bool[2]); // Acessando o terceiro elemento
+
+console.log(arr[arr.length - 1]); // Essa é uma maneira de acessar o último elemento mesmo sem saber quantos elementos existem
