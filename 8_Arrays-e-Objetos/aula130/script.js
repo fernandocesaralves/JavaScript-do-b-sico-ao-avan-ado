246c394e072634eb5204a83201389426ea06fc45
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-11-20 20:11:27 -0300

    Arrays

---
+++ 8_Arrays-e-Objetos/aula130/script.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+let nomes = ['Fernando', 'Filomena', 'João', 'Thais'];
+
+let bool = [true, false, true, false];
+
+
+let misturado = [1, 'Fernando', true];
+
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado);
