b1003634fa6f587e5db0eed724a7dfe38e7a395a
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-11-20 01:07:27 -0300

    Exercicio 7

---
+++ 7_Exercicios_sobre_funcoes/aula120_exercicio/script.js
@@ -0,0 +1,10 @@
+// Escreva uma função que receba um número negativo e retorne um número positivo
+
+// Dica utlize a função Math.abs
+
+function transformarNumero(n) {
+    console.log(Math.abs(n));
+
+}
+
+transformarNumero(-3);
---
+++ 7_Exercicios_sobre_funcoes/aula121_solucao/script.js
@@ -0,0 +1,9 @@
+function numeroPos(numeroNeg) {
+    return Math.abs(numeroNeg);
+}
+
+console.log(numeroPos(-5));
+console.log(numeroPos(-4));
+console.log(numeroPos(-13));
+console.log(numeroPos(5));
+console.log(numeroPos(-34.4));
