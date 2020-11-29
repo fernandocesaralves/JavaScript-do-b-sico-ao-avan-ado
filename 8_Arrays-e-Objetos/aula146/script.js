c90482bb999a20f43f154f50c311843305beefaf
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-11-29 03:17:59 -0300

    Metodos de array reverse

---
+++ 8_Arrays-e-Objetos/aula146/script.js
@@ -0,0 +1,10 @@
+// Iverte um array
+
+let carros = ["BMW", "Fiat", "VW", "Renaul", "Audi"];
+
+console.log(carros.reverse());
+
+
+let nums = [1, 2, 3, 45, 6, 7, 7];
+
+console.log(nums.reverse());
