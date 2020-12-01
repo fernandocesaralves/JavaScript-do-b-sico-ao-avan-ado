5493062dd00a7153d8d13fe21dc1fd65ef4560eb
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-12-01 05:27:25 -0300

    metodos de string: padStart, split, join, repeat

---
+++ 8_Arrays-e-Objetos/aula149/script.js
@@ -0,0 +1,13 @@
+// Divide uma string por um separador, e retorna um array
+
+let frase = "Testando o método split";
+
+console.log(frase.split(" "));
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let produtos = "Banana;Jaca;Maçã;Uva;Bola;Tapete";
+
+console.log(produtos.split(";"));
---
+++ 8_Arrays-e-Objetos/aula151/script.js
@@ -0,0 +1,5 @@
+// Repete uma string de acordo com um parâmetro number
+
+let palavra = "repetir ";
+
+console.log(palavra.repeat(2));
