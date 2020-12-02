1f0b3caf7513385f24436afe54c9acc4eb3b3ff5
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-12-02 10:22:23 -0300

    O que e JSON e Destructing em arrays

---
+++ 8_Arrays-e-Objetos/aula155/script.js
@@ -0,0 +1,13 @@
+// JSON - JavaScript Object Notation 
+
+// Utilizado para comunicação entre serviços, ex: back end <-> front end
+
+// Basicamente um tipo de dado padronizado, que lembra muito os objetos do javaScript
+
+let pessoa = {
+    "nome": "Fernando",
+    "idade": 27
+}
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
