f91623027ad35f6831c1977a94812661e3e77e61
FernandoCesar <fernandocesar.alves@hotmail.com>
2020-12-09 15:33:15 -0300

    Override no prototype

---
+++ 10_Conceitos_de_orientacao_a_objetos/aula192/script.js
@@ -0,0 +1,27 @@
+// Sempre que adicionamos uma propriedade a um objeto, é criada uma idêntica no Prototype
+
+// Podemos substituir a do prototype
+
+class Cachorro {
+    constructor(raca, cor) {
+        this.raca = raca;
+        this.cor = cor;
+    }
+
+    latir() {
+        console.log("Au, au, au!");
+    }
+}
+
+
+Cachorro.prototype.raca = "SRD";
+Cachorro.prototype.patas = 4;
+
+let labrador = new Cachorro("Labrador", "Amarelo");
+
+console.log(labrador.patas);
+
+labrador.latir();
+
+console.log(Cachorro.prototype.raca);
+console.log(labrador.raca);
