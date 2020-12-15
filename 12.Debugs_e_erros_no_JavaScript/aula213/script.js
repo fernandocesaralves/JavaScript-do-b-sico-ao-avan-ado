0274e4de155b67031d272c344e159a60a6d45c11
Fernando César <fernandocesar.alves@hotmail.com>
2020-12-15 12:11:04 -0300

    Strict, console.log e debugger

---
+++ 12.Debugs_e_erros_no_JavaScript/aula213/script.js
@@ -0,0 +1,22 @@
+// Deixa o javaScript mais rigoroso na hora de se programar
+
+// Deve ser declarado no topo de arquivos ou funções
+
+// Colocar o strict ajuda você a codificar de forma correta e não vai impedir/limitar nada no seu software ou programar
+
+"use strict"
+
+let opa = "teste";
+
+// delete Object.prototype;
+
+function teste() {
+    "use strict"
+    let testando = "teste";
+}
+
+teste();
+
+// false.prop = "";
+// "teste".prop = "";
+
