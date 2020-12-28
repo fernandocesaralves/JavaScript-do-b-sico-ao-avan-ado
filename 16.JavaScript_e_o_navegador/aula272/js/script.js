c022532a9ce09ac73372af75afb333ea7a086246
Fernando César <fernandocesar.alves@hotmail.com>
2020-12-28 20:08:23 -0300

    movendo elementos html com DOM

---
+++ 16.JavaScript_e_o_navegador/aula272/js/script.js
@@ -0,0 +1,15 @@
+// Podemos acessar todos os elementos a partir de document.body
+
+// A partir dele vamos entrando nos childNodes
+
+// E depois acessando as propriedades que nos interessam 
+
+console.log(document.body);
+
+console.log(document.body.childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes);
+
+console.log(document.body.childNodes[1].childNodes[1]);
+
+console.log(document.body.childNodes[1].childNodes[1].innerText);
