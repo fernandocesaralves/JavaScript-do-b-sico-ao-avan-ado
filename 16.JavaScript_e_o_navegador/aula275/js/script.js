bbd8e2657ec91c11c77bbb36059433bdf9b2391c
Fernando César <fernandocesar.alves@hotmail.com>
2020-12-30 04:26:32 -0300

    encontrando elementos por Id e Class

---
+++ 16.JavaScript_e_o_navegador/aula275/js/script.js
@@ -0,0 +1,6 @@
+// Utilizamos o seguinte método para encontrar elementos por id
+
+// document.getElementById('algum-id);
+
+console.log(document.getElementsByTagName('h1'));
+console.log(document.getElementById('titulo-principal'));
---
+++ 16.JavaScript_e_o_navegador/aula276/js/script.js
@@ -0,0 +1,11 @@
+// Utilizamos o seguinte método para encontrar elementos por classe 
+
+// document.getElementsByClassname('alguma-classe');
+
+console.log(document.getElementsByTagName('li'));
+
+console.log(document.getElementsByClassName('itens-azuis'));
+
+console.log(document.getElementsByClassName('itens-vermelhos'));
+
+console.log(document.getElementsByClassName('itens'));
