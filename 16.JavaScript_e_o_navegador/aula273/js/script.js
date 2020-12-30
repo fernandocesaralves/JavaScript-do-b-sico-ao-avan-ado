1db263589847d52047c6bf605a3db34e77a43d28
Fernando César <fernandocesar.alves@hotmail.com>
2020-12-30 03:36:19 -0300

    encontrando elementos por tag

---
+++ 16.JavaScript_e_o_navegador/aula273/js/script.js
@@ -0,0 +1,6 @@
+// Podemos encontrar elementos de uma forma mais fácil do que se movendo pelo DOM
+
+// Como no HTML temos tags, classes e ids, o javascript nos possibilita utilizar estas características para encontrar o que desejamos
+
+// Há alguns métodos para isso: getElementsByTagName, getElementById, getElementsByClassName, e também o querySelector
+
---
+++ 16.JavaScript_e_o_navegador/aula274/js/script.js
@@ -0,0 +1,7 @@
+// Utilizamos o seguinte método para encontrar elementos pela tag
+
+// document.getElementByTagName('h1);
+
+console.log(document.getElementsByTagName('h1'));
+console.log(document.getElementsByTagName('p'));
+console.log(document.getElementsByTagName('li'));
