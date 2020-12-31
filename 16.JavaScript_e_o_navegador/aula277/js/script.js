4ed8d521c369635b3986968b94d92e4f1a2e3dcb
Fernando César <fernandocesar.alves@hotmail.com>
2020-12-31 02:58:36 -0300

    encontrando elementos por query

---
+++ 16.JavaScript_e_o_navegador/aula277/js/script.js
@@ -0,0 +1,15 @@
+// Um método que se assemelha a grande funcionalidade do JQuery
+
+// Nos permitindo encontrar elementos pelo seus seletores do CSS
+
+// O que facilita muito nossa vida 
+
+console.log(document.querySelector('#container-principal h1'));
+
+console.log(document.querySelector('#segundo-container h1'));
+
+console.log(document.querySelector('div div p'));
+
+console.log(document.querySelector('footer h2'));
+
+console.log(document.querySelector('ul .itens-azuis'));
