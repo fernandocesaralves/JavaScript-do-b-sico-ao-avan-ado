b1438349bb1466b17f9408bb0181ff79eac70c8a
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-05 04:21:06 -0300

    stopPropagation

--- 17.Eventos_com_JavaScript/aula295/js/script.js
+++ 17.Eventos_com_JavaScript/aula295/js/script.js
@@ -1,17 +1,26 @@
-// Da mesma forma que podemos adicionar eventos, podemos remover quando achamos necessário 
+// Quando não definimos um elemento muito bem (seletor brando) ou um elemento que está dentro de outro tem um evento 
 
-// Para isso utilizamos o método removeEventListener, onde passamos o evento e a função que o evento está escutando 
+// Pode acontecer a propagação, ou seja, o outro elemento ativar um evento tanmbém, aí teremos uma duplicação 
+
+// Por isso temos um método que para esta propagação e resolve este problema, o stopPropagation
 
 let btn1 = document.querySelector("#btn1");
 let btn2 = document.querySelector("#btn2");
+let p = document.querySelector("p");
 
-function msg() {
-    console.log("Clicou em mim!");
+function msg(e) {
+    console.log("Clicou no botão");
+    e.stopPropagation();
 }
 
 btn1.addEventListener("click", msg);
 
-btn2.addEventListener("click", function() {
-    btn1.removeEventListener("click", msg);
+btn2.addEventListener("click", function(event) {
+    console.log(event);
 });
+
+p.addEventListener("click", function() {
+    console.log("Clicou no parágrafo");
+});
+
 
