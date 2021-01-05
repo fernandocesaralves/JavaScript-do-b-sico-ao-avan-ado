40d64bdc5214549f5aab66ec3b1356654c622b9c
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-05 16:45:21 -0300

    Event default

---
+++ 17.Eventos_com_JavaScript/aula296/js/script.js
@@ -0,0 +1,32 @@
+// Muitos elementos/teclas já tem ações pre-definidas, como clicar num link nos leva a outra página 
+
+// Podemos parar este evento default, e criar uma lógica diferente para o elemento em questão 
+
+let btn1 = document.querySelector("#btn1");
+let btn2 = document.querySelector("#btn2");
+let p = document.querySelector("p");
+let a = document.querySelector("a");
+
+function msg(e) {
+    console.log("Clicou no botão");
+    e.stopPropagation();
+}
+
+btn1.addEventListener("click", msg);
+
+btn2.addEventListener("click", function(event) {
+    console.log(event);
+});
+
+p.addEventListener("click", function() {
+    console.log("Clicou no parágrafo");
+});
+
+a.addEventListener('click', function(e) {
+
+    e.preventDefault();
+
+    console.log("Não vai mudar de link");
+
+});
+
