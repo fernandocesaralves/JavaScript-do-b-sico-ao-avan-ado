b078a5c12fcec6b242a1d83f8dfd20f531d6732c
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-11 21:14:06 -0300

    npm init

---
+++ 19.Aprendendo_Node.js/aula320/index.js
@@ -0,0 +1,14 @@
+// Iniciando um orjeto
+
+// Sempre precisamos ter um pacote package.json 
+
+// Podemos criar a partir do template, utilizando o comando: npm init
+
+// Aí teremos um projeto com o package.json
+
+// Este arquivo serve para configurações e salva nossas dependências 
+
+let msg = "Hello world!";
+
+console.log(msg);
+
