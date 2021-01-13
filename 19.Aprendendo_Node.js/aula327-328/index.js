7967e639c22e37787aa310da32cf6d09dcc77c0f
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-13 00:19:11 -0300

    express

---
+++ 19.Aprendendo_Node.js/aula327-328/index.js
@@ -0,0 +1,17 @@
+// Express
+
+// Um framework web, muito utilizado para criar aplicações em Node.js
+
+// Utiliza em seu core o módulo HTTP para criar as comunicações
+
+// Além de outros módulos
+
+// Podemos criar uma API com o Express facilmente
+
+// O Express precisa ser intslado
+
+// nmp init 
+
+// npm install express
+
+
